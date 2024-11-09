db5160eb678c2d6ee91e7cee230e7e7b13cd9d54
preciousyaks17 <johndoe@example.com>
2024-11-09 23:50:41 +0100

    core: worked on user storage

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,7 +1,11 @@
-import { View, StyleSheet, Button } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { theme } from "../theme";
 import { useUserStore } from "../store/userStore";
 import { useRouter } from "expo-router";
+import { PlantlyButton } from "../components/PlantlyButton";
+import { StatusBar } from "expo-status-bar";
+import { PlantlyImage } from "../components/PlantlyImage";
 export default function OnboardingScreen() {
   const router = useRouter();
   const toggleHassOnboarded = useUserStore((state) => state.toggleHasOnboarded);
@@ -10,20 +14,47 @@ export default function OnboardingScreen() {
     router.replace("/");
   };
   return (
-    <View style={styles.container}>
-      <Button title="Let me in " onPress={handlePress} />,
-    </View>
+    <LinearGradient
+      start={{ x: 0, y: 0 }}
+      end={{ x: 3, y: 1 }}
+      colors={[theme.colorGreen, theme.colorAppleGreen, theme.colorLimeGreen]}
+      style={styles.container}
+    >
+      <StatusBar style="light" />
+      <View>
+        <Text style={styles.heading}>Plantly</Text>
+        <Text style={styles.tagLine}>
+          Keep your plants healthy and hydrated
+        </Text>
+      </View>
+      <PlantlyImage />
+      <PlantlyButton title="Let me in " onPress={handlePress} />,
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "space-evenly",
     alignItems: "center",
     backgroundColor: theme.colorWhite,
+    paddingHorizontal: 8,
   },
   text: {
     fontSize: 24,
+  },
+  heading: {
+    fontSize: 42,
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    marginBottom: 12,
+    textAlign: "center",
+  },
+
+  tagLine: {
+    fontSize: 12,
+    color: theme.colorWhite,
+    textAlign: "center",
   },
 });
